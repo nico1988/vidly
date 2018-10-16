034a8604dc726e04ed4115e0d2e1eba619db5b51
Lissette Bustos Mora <lissette.hoon@gmail.com>
2018-10-15 23:18:19 -0300

    extracting the config logic

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-
-const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
 require('./startup/logging')();
+require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-if(!config.get('jwtPrivateKey')){
-    console.log("FATAL ERROR!!!, PrivateKey Undefined");
-    process.exit(1);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
