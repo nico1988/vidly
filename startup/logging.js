5d38ba10d63d462b39b2f9992840cb90ac548b51
Lissette Bustos Mora <lissette.hoon@gmail.com>
2018-10-16 00:00:56 -0300

    add winston.transport.console

--- startup/logging.js
+++ startup/logging.js
@@ -2,12 +2,17 @@ const winston = require('winston');
 require('express-async-errors'); // No es necesario guardarlo en una const 
 
 module.exports = function(){
-    winston.add(new winston.transports.File({
-        filename: 'logfile.log',
-        handleExceptions: true
-    }));
+    winston.add(
+        new winston.transports.File({ filename: 'logfile.log', handleExceptions: true })
+    );
 
     winston.exceptions.handle( 
+        new winston.transports.Console({ 
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.json()
+            )
+        }),
         new winston.transports.File({ filename: 'uncaughtExceptions.log' })
     );
     
