27067affdf4d324529d74ba90ca850b1a68413e7
Lissette <lissette.bustos.mora@gmail.com>
2018-10-17 14:10:12 -0300

    First integration test on genres

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ require('./startup/db')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Running on ${port}....`);
     //winston.info(`Running on ${port}....`);
 })
+
+module.exports = server;
+
 
 
